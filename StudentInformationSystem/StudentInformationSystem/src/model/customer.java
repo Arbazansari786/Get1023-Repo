5f69395671ca1bd85d5b05db8f7540c0436ead05
salamansari444 <salamansari444@gmail.com>
2022-07-13 13:00:48 +0530

    third commit

--- StudentInformationSystem/StudentInformationSystem/src/model/customer.java
+++ StudentInformationSystem/StudentInformationSystem/src/model/customer.java
@@ -1,9 +1,37 @@
 package model;
 
 public class customer {
+	int cid;
+	int name;
 	
+	
+	public int getCid() {
+		return cid;
+	}
+
+
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+
+
+	public int getName() {
+		return name;
+	}
+
+
+	public void setName(int name) {
+		this.name = name;
+	}
+
+
 	public static void main(String args[]) {
 		System.out.println("hi");
+		
+		int customerid;
+		int age;
+		
+		
 	}
 
 }
